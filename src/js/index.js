1abd139090f3890aa61942a07c8e18611d6c990a
Max <maxpachinov@gmail.com>
2019-06-10 17:21:41 +0300

    first steps in design

--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,6 @@ import _ from 'lodash';
 
 import './../sass/styles.scss';
 
-const getHeader = () => {
-  const helloWebpack = _.join(['Hello', 'webpack!'], ' ');
-  console.log(helloWebpack);
-  const element = document.createElement('h1');
-
-  element.innerHTML = helloWebpack;
-
-  return element;
-};
-
-document.body.appendChild(getHeader());
-
 const o = {
   foo: {
     bar: null
