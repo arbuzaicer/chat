bb96436037b4e152db0434fc0b78358288f8e6c8
arbuzaicer <maxpachinov@gmail.com>
2019-06-11 15:38:06 +0300

    Creating a head section

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,13 @@ const o = {
   }
 };
 
+(function setDate() {
+  let date = new Date();
+  let dateImport = document.querySelector('.current_date');
+  let currentHour = date.getHours();
+  let currentMinutes = date.getMinutes();
+  let currentSeconds = date.getSeconds();
+  dateImport.innerHTML = currentHour+':ч '+currentMinutes+':м '+currentSeconds+':с';
+}());
+
 console.log(o?.foo?.bar?.baz ?? 'default');
