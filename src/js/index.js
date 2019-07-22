49f51f4f00bb5cbee09263ed8788ad73505cb771
arbuzaicer <49945177+arbuzaicer@users.noreply.github.com>
2019-07-22 19:45:34 +0300

    Version with objects. Checking new messages

--- src/js/index.js
+++ src/js/index.js
@@ -17,12 +17,10 @@ var usersInform = document.querySelector(".users_contacts_section");
 var paricipantName = document.querySelector(".name_user");
 var chatStatus = document.querySelector(".chat_status");
 var chatRoom = document.querySelector("#chatroom");
-
 var usersMsg = document.querySelector("#chat1");
 var sendMessage = document.querySelector(".send_message");
 var msgText = document.querySelector("#message_section");
 var usersChatSwitcher = document.querySelector(".msg_users");
-
 
 var totalSymbols = document.querySelector(".Symbols");
 var invis = document.querySelector("#invisible");
@@ -34,43 +32,76 @@ var msgDate = new Date();
 
     /*Function that create an users array from JSON*/
 
-function GetUsersArray(users_url) {
+var authentication = {
+    getMyId: function() {
 
-    let users_array = new XMLHttpRequest();
-    users_array.open("GET", users_url, false);
-    users_array.send(null);
+        for(let i = 0; i < usersArray.length; i++) {
 
-    return users_array.responseText;
-}
+            if(usersArray[i].username === name1.value) {
 
-var usersArray = JSON.parse(GetUsersArray("https://studentschat.herokuapp.com/users/"));
+                return usersArray[i].user_id;
+            }
+
+        }
+    },
+    getUsersName: function () {
+
+        let users = new XMLHttpRequest();
+        users.open("GET", "https://studentschat.herokuapp.com/users/", false);
+        users.send(null);
+        let usersArray = JSON.parse(users.responseText);
+        let userUsernames = [];
+
+        for (let i = 0; i < usersArray.length; i++) {
+            userUsernames[i]= usersArray[i].username;
+        }
+
+        return userUsernames;
+    },
+    GetUsersArray: function (users_url) {
+
+        let users_array = new XMLHttpRequest();
+        users_array.open("GET", users_url, false);
+        users_array.send(null);
+
+        return users_array.responseText;
+    },
+    getMessages: function () {
+
+    let messages_Array = new XMLHttpRequest();
+    messages_Array.open("GET", "https://studentschat.herokuapp.com/messages", false);
+    messages_Array.send(null);
+
+    return messages_Array.responseText;
+    }
+};
+
+var usersArray = JSON.parse(authentication.GetUsersArray("https://studentschat.herokuapp.com/users/"));
+var sortStatusUsers = usersArray.sort(sorting);
+var messagesArray = JSON.parse(authentication.getMessages());
+var fullUsersArray = authentication.getUsersName();
+var messagesData = JSON.parse(authentication.getMessages());
 
 /*Sorting users for status parameter*/
 
 function sorting(a, b) {
+
     if(a["status"] > b["status"]) {
+
         return 1;
     } else {
+
         return -1;
     }
 
 }
 
-/*Creating a status sorted array*/
+/*Setting user Information*/
 
-var sortStatusUsers = usersArray.sort(sorting);
-
-/*Function that set my ID*/
-
-function getMyId() {
-
-    for(let i = 0; i < usersArray.length; i++) {
-
-        if(usersArray[i].username === name1.value) {
-            return usersArray[i].user_id;
-        }
-    }
-
+function setUserInfo() {
+    let name = name1.value;
+    let setName = document.querySelector('#userName');
+    setName.innerHTML = name;
 }
 
 /*Modal*/
@@ -102,217 +133,237 @@ register.onclick = function() {
 
 signButton.onclick = function () {
 
-    let request = new XMLHttpRequest();
-    request.open('GET', 'https://studentschat.herokuapp.com/users', true);
+    if(fullUsersArray.includes(name1.value)) {
+        alert("Hello "+name1.value);
+        bodyChat.style.display = "block";
+        modalOut.style.display = "none";
 
-    request.onload = function() {
+        setUserInfo();
 
-        if (request.status >= 200 && request.status < 400) {
-            // Обработчик успещного ответа
+    } else {
 
-            var response = request.responseText;
-            var userList = JSON.parse(response);
-            var usersArray = [];
+        alert("There is no USER with name: "+name1.value+" .Please Enter Your name and Click REGISTER button.");
 
-            for(let i = 0; i < userList.length; i++) {
-                usersArray[i]=userList[i].username;
-            }
+    }
 
-            /*Creating authentication form.*/
+    function creatingDomUsers() {
+        let request = new XMLHttpRequest();
 
-            if(usersArray.includes(name1.value)) {
-                alert("Hello "+name1.value);
-                bodyChat.style.display = "block";
-                modalOut.style.display = "none";
+        request.open('GET', 'https://studentschat.herokuapp.com/users', true);
 
-                (function setUserInfo() {
-                    let name = name1.value;
-                    let setName = document.querySelector('#userName');
-                    setName.innerHTML = name;
-                }());
+        request.onload = function() {
+
+            if (request.status >= 200 && request.status < 400) {
+                // Обработчик успещного ответа
+
+                var response = request.responseText;
+                var userList = JSON.parse(response);
+                var usersArray = [];
+
+                for(let i = 0; i < userList.length; i++) {
+                    usersArray[i]=userList[i].username;
+                }
+
+                /*Creating authentication form.*/
+
+
+                for(let i = 0; i < sortStatusUsers.length; i++) {
+                    let currentUser = document.createElement("div");
+                    let linkImage = document.createElement("IMG");
+                    let linkUser = document.createElement("a");
+
+                    if(i%2===0) {
+                        currentUser.className = "participants parteven";
+                    } if(i%2!==0) {
+                        currentUser.className = "participants partodd";
+                    }
+
+                    /*Creating users using DOM*/
+                    if(sortStatusUsers[i].username===name1.value) {
+                        continue;
+                    }
+                    /*Creating a participants block*/
+
+                    usersInform.appendChild(currentUser);
+
+                    /*Creating a user Image*/
+
+                    let userName = document.createTextNode(sortStatusUsers[i].username);
+                    linkImage.className = "chat_users";
+                    linkImage.setAttribute("src", "http://clipart-library.com/images/dc45n8yzi.png");
+                    linkImage.setAttribute("alt", "Man_user"+i);
+                    currentUser.appendChild(linkImage);
+
+                    /*Creating a link with user Name block*/
+
+                    linkUser.className = "users_names";
+                    linkUser.title = sortStatusUsers[i].username;
+                    linkUser.href = "#";
+                    linkUser.appendChild(userName);
+                    currentUser.appendChild(linkUser);
+
+                    /*Creating a user Status block*/
+
+                    let userStatus = document.createElement("p");
+                    let statusName = document.createTextNode(sortStatusUsers[i].status);
+
+                    if(sortStatusUsers[i].status==="active") {
+                        userStatus.className = "online_in_chat chat_status";
+                        userStatus.appendChild(statusName);
+                        currentUser.appendChild(userStatus);
+                    }
+
+                    if(sortStatusUsers[i].status==="inactive") {
+                        userStatus.className = "contacts_in_chat chat_status";
+                        userStatus.appendChild(statusName);
+                        currentUser.appendChild(userStatus);
+                    }
+
+                }
+
+                /*End cycle*/
+
+                totalUsers.innerHTML = sortStatusUsers.length;
 
             } else {
-
-                alert("There is no USER with name: "+name1.value+" .Please Enter Your name and Click REGISTER button.");
-
+                // Обработчик ответа в случае ошибки
             }
+        };
 
-            for(let i = 0; i < sortStatusUsers.length; i++) {
-                let currentUser = document.createElement("div");
-                let linkImage = document.createElement("IMG");
-                let linkUser = document.createElement("a");
+        request.onerror = function() {
 
-                if(i%2===0) {
-                    currentUser.className = "participants parteven";
-                } if(i%2!==0) {
-                    currentUser.className = "participants partodd";
-                }
+            alert("network is unstable. Please check your internet connection.")
 
-                /*Creating users using DOM*/
-                if(sortStatusUsers[i].username===name1.value) {
-                    continue;
-                }
-                /*Creating a participants block*/
+        };
 
-                usersInform.appendChild(currentUser);
+        request.send();
+    }
 
-                /*Creating a user Image*/
+    creatingDomUsers();
 
-                let userName = document.createTextNode(sortStatusUsers[i].username);
-                linkImage.className = "chat_users";
-                linkImage.setAttribute("src", "http://clipart-library.com/images/dc45n8yzi.png");
-                linkImage.setAttribute("alt", "Man_user"+i);
-                currentUser.appendChild(linkImage);
+    var currTime = new Date(messagesData[messagesData.length-1].datetime);
 
-                /*Creating a link with user Name block*/
-
-                linkUser.className = "users_names";
-                linkUser.title = sortStatusUsers[i].username;
-                linkUser.href = "#";
-                linkUser.appendChild(userName);
-                currentUser.appendChild(linkUser);
-
-                /*Creating a user Status block*/
-
-                let userStatus = document.createElement("p");
-                let statusName = document.createTextNode(sortStatusUsers[i].status);
-
-                if(sortStatusUsers[i].status==="active") {
-                    userStatus.className = "online_in_chat chat_status";
-                    userStatus.appendChild(statusName);
-                    currentUser.appendChild(userStatus);
-                }
-
-                if(sortStatusUsers[i].status==="inactive") {
-                    userStatus.className = "contacts_in_chat chat_status";
-                    userStatus.appendChild(statusName);
-                    currentUser.appendChild(userStatus);
-                }
-
-            }
-
-            /*End cycle*/
-
-            totalUsers.innerHTML = sortStatusUsers.length;
-
-        } else {
-            // Обработчик ответа в случае ошибки
-        }
+    var lastMsg = {
+        lastMsgMin: currTime.getMinutes(),
+        lastMsgSec: currTime.getSeconds()
     };
 
-    request.onerror = function() {
-
-        alert("network is unstable. Please check your internet connection.")
-
-    };
-
-    request.send();
-
-    var totalMsg = new XMLHttpRequest();
-
-    totalMsg.open('GET', 'https://studentschat.herokuapp.com/messages', true);
-
-    totalMsg.send();
-
-    totalMsg.onreadystatechange = function() {
-        if (this.readyState != 4) return;
-
-        // по окончании запроса доступны:
-        // status, statusText
-        // responseText, responseXML (при content-type: text/xml)
-
-        if (this.status != 200) {
-            // обработать ошибку
-            alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
-            return;
-        }
-
-        var getMessages = totalMsg.responseText;
-        var messagesData = JSON.parse(getMessages);
+    function creatingDomMessages() {
         var total_msg, user_info, dialog, msgData, textInfo, userFirstName,  msgDateHour, msgMinutes, msgTime, currentTime,
-        currentDate;
+            currentDate;
 
 
-        for(let i = 0; i < messagesData.length; i++){
-            currentDate = document.createElement("div");
-            currentDate.className="dataMessages";
-            total_msg = document.createElement("div");
-            total_msg.className = "total_msg";
-            user_info = document.createElement("div");
-            user_info.className = "user_info msgs";
-            dialog = document.createElement("div");
-            dialog.className = "dialog msgs";
-            let linkImage = document.createElement("IMG");
+        console.log(lastMsg.lastMsgMin+" "+lastMsg.lastMsgSec);
 
-            if(messagesData[i].user_id == getMyId()) {
-                linkImage.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:" +
-                    "ANd9GcTX1PNJ2ZQWmCvQseFXkP6qx6MRQjbq4Yb45Ig29YdY0faKhi96");
-            } else {
-                linkImage.setAttribute("src", "http://www.free-icons-download.net/images/administrator-icon-5154.png");
-            }
+        (function drawingMsgs () {
 
-            linkImage.setAttribute("alt", "Man_user3"+i);
-            linkImage.className = "chat_users";
+            for(let i = 0; i < messagesArray.length; i++){
+                currentDate = document.createElement("div");
+                currentDate.className="dataMessages";
+                total_msg = document.createElement("div");
+                total_msg.className = "total_msg";
+                user_info = document.createElement("div");
+                user_info.className = "user_info msgs";
+                dialog = document.createElement("div");
+                dialog.className = "dialog msgs";
+                let linkImage = document.createElement("IMG");
 
-            let userName = document.createElement("p");
-            userName.className = "name_user";
-
-            for(let j = 0; j < usersArray.length; j++) {
-
-                if(messagesData[i].user_id === usersArray[j].user_id) {
-                    userFirstName = usersArray[j].username;
-                }
-
-            }
-
-            userName.innerHTML =(userFirstName);
-
-            msgTime = document.createElement("span");
-            msgTime.className = "msg_time";
-
-            msgDate = new Date(messagesData[i].datetime);
-            msgDateHour = msgDate.getHours();
-            msgMinutes = msgDate.getMinutes();
-            currentTime = msgDateHour+" : "+msgMinutes;
-            msgTime.innerHTML = currentTime;
-
-
-            msgData = document.createElement("p");
-            textInfo = messagesData[i].message;
-
-            if(messagesData[i]["datetime"]) {
-                msgData.appendChild(msgTime);
-                msgData.innerHTML = textInfo;
-
-                user_info.appendChild(linkImage);
-                user_info.appendChild(userName);
-                dialog.appendChild(msgData);
-                msgData.appendChild(msgTime);
-
-                chatRoom.innerHTML = messagesData[i].chatroom_id;
-
-                if(messagesData[i].user_id == getMyId()) {
-                    msgData.className = "your_text";
-                    total_msg.appendChild(dialog);
-                    total_msg.appendChild(user_info);
-                    currentDate.appendChild(total_msg);
-                    usersMsg.appendChild(currentDate);
+                if(messagesArray[i].user_id == authentication.getMyId()) {
+                    linkImage.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:" +
+                        "ANd9GcTX1PNJ2ZQWmCvQseFXkP6qx6MRQjbq4Yb45Ig29YdY0faKhi96");
                 } else {
-                    msgData.className = "friend_text";
-                    total_msg.appendChild(user_info);
-                    total_msg.appendChild(dialog);
-                    currentDate.appendChild(total_msg);
-                    usersMsg.appendChild(currentDate);
+                    linkImage.setAttribute("src", "http://www.free-icons-download.net/images/administrator-icon-5154.png");
                 }
+
+                linkImage.setAttribute("alt", "Man_user3"+i);
+                linkImage.className = "chat_users";
+
+                let userName = document.createElement("p");
+                userName.className = "name_user";
+
+                for(let j = 0; j < usersArray.length; j++) {
+
+                    if(messagesArray[i].user_id === usersArray[j].user_id) {
+                        userFirstName = usersArray[j].username;
+                    }
+
+                }
+
+                userName.innerHTML =(userFirstName);
+
+                msgTime = document.createElement("span");
+                msgTime.className = "msg_time";
+
+                msgDate = new Date(messagesData[i].datetime);
+                msgDateHour = msgDate.getHours();
+                msgMinutes = msgDate.getMinutes();
+                let curDate = msgDate.getDate();
+
+                currentTime = msgDateHour+" : "+msgMinutes;
+                msgTime.innerHTML = currentTime;
+
+
+                msgData = document.createElement("p");
+                textInfo = messagesData[i].message;
+
+                if(msgDate.getDate()===curDate) {
+                    msgData.appendChild(msgTime);
+                    msgData.innerHTML = textInfo;
+
+                    user_info.appendChild(linkImage);
+                    user_info.appendChild(userName);
+                    dialog.appendChild(msgData);
+                    msgData.appendChild(msgTime);
+
+                    chatRoom.innerHTML = messagesData[i].chatroom_id;
+
+                    if(messagesArray[i].user_id == authentication.getMyId()) {
+                        msgData.className = "your_text";
+                        total_msg.appendChild(dialog);
+                        total_msg.appendChild(user_info);
+                        currentDate.appendChild(total_msg);
+                        usersMsg.appendChild(currentDate);
+                    } else {
+                        msgData.className = "friend_text";
+                        total_msg.appendChild(user_info);
+                        total_msg.appendChild(dialog);
+                        currentDate.appendChild(total_msg);
+                        usersMsg.appendChild(currentDate);
+                    }
+                }
+
+
             }
 
-        }
+            /*Scrolling our message box to bottom*/
 
-        /*Scrolling our message box to bottom*/
+            usersMsg.scrollTo(0, 1000);
 
-        usersMsg.scrollTo(0, 1000);
-
+        }());
     };
+
+    creatingDomMessages();
+
+
+
+
+        function getNewMsgs () {
+            var checkingNewMessages = setInterval(function() {
+                var messagesData = JSON.parse(authentication.getMessages());
+                var currTime = new Date(messagesData[messagesData.length-1].datetime);
+                var lastMsgNew = {
+                    lastMsgMin: currTime.getMinutes(),
+                    lastMsgSec: currTime.getSeconds()
+                };
+
+                if((lastMsg.lastMsgMin && lastMsg.lastMsgSec)!==(lastMsgNew.lastMsgMin && lastMsgNew.lastMsgSec)) {
+
+                    /*Какое условие проверки мне надо сюда написать?*/
+
+                }
+
+            }, 5000)
+        }
 
 };
 
@@ -340,7 +391,7 @@ sendMessage.onclick = function () {
     msgSend.setRequestHeader('Content-Type', 'application/json');
 
     msgSend.send(JSON.stringify({
-        user_id: getMyId(),
+        user_id: authentication.getMyId(),
         message: msgText.value,
         datetime: msgDate
     }));
@@ -452,5 +503,5 @@ sendMessage.onclick = function () {
             return valString;
         }
     }
-}())
+}());
 
